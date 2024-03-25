3732a858f8a6dfa75a5372eadd750d03cbd27515
Muhammad Bintang Al-Fath <alfathbintangmuhammad@gmail.com>
2024-03-25 11:07:37 +0700

    fix artikel and video page

--- lib/app/modules/video_page/views/video_page_view.dart
+++ lib/app/modules/video_page/views/video_page_view.dart
@@ -12,62 +12,9 @@ class VideoPageView extends GetView<VideoPageController> {
   const VideoPageView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         backgroundColor: Color(0xFFf8f8f8),
-        appBar: AppBar(
-            toolbarHeight: 100,
-            automaticallyImplyLeading: false,
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Edukasi',
-                  style: GoogleFonts.poppins(
-                      textStyle: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black)),
-                ),
-                SizedBox(height: 15),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () {
-                            Get.to(ArticlePageView());
-                          },
-                          child: Container(
-                            child: Center(
-                              child: Text(
-                                'Artikel',
-                                style: TextStyle(color: Color(0xff88c0f2)),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(
-                                    color: Color(0xff88c0f2), width: 3)),
-                          ),
-                          child: Center(
-                            child: Text(
-                              'Video',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  color: Color(0xff88c0f2)),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            )),
+        
         body: SingleChildScrollView(
           child: Column(
             children: [
